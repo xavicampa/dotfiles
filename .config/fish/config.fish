66241aba5349664e74e0444cc9a206e3729bd637
Xavi Campa <xavi@campalus.com>
2021-11-17 10:13:10 +0100

    Removed fish startup message

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,6 +2,7 @@ if status --is-login
     set -gx PATH $PATH ~/.local/bin
     set -gx PATH $PATH ~/.dotnet/tools
     set -Ux FZF_DEFAULT_COMMAND 'fdfind --type f'
+    set -U fish_greeting
 end
 
 alias dotfiles="/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME"
