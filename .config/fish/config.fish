8b5e1da689ad438f8c688464481f4132686e24bb
Xavi Campa <xavi@campalus.com>
2021-11-21 16:20:47 +0100

    exa config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,8 +1,15 @@
 alias dotfiles="/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME"
+if test -f /usr/bin/exa
+    alias l='exa'
+    alias la='exa -a'
+    alias ll='exa -lah'
+    alias ls='exa --color=auto'
+end
 
 if status --is-login
     set -gx PATH $PATH ~/.local/bin
     set -gx PATH $PATH ~/.dotnet/tools
+    set -gx PATH $PATH ~/.cargo/bin
     set -U fish_greeting
     set -Ux FZF_DEFAULT_COMMAND 'fdfind --type f'
     set -Ux EDITOR vim
