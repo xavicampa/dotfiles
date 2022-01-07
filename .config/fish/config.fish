2f026c4b4d03a114a1610b039f2e90f0fd12a5ec
Xavi Campa <xavi@campalus.com>
2022-01-07 17:43:22 +0100

    Fish and alacritty changes for macos

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -10,10 +10,15 @@ if test -f /home/javi/nvim.appimage
     alias vim='/home/javi/nvim.appimage'
 end
 
+if which nvim>/dev/null
+    alias vim='/opt/homebrew/bin/nvim'
+end
+
 if status --is-login
     set -gx PATH $PATH ~/.local/bin
     set -gx PATH $PATH ~/.dotnet/tools
     set -gx PATH $PATH ~/.cargo/bin
+    set -gx PATH $PATH /Users/javi/Library/Python/3.8/bin
     set -U fish_greeting
     set -Ux EDITOR vim
     set -Ux VISUAL $EDITOR
