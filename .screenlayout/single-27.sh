37de7489cd202275fed5656794e2f7876e8c7605
Xavi Campa <javi@campalus.com>
2022-07-10 21:01:28 +0200

    screenlayout changes

--- .screenlayout/single-27.sh
+++ .screenlayout/single-27.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
 xrandr --output HDMI-0 --off --output DP-0 --primary --mode 3840x2160 --rate 119.91 --pos 0x0 --rotate normal --output DP-1 --off --output DP-2 --off --output DP-3 --off --output DP-4 --off --output DP-5 --off --output DP-1-1 --off --output HDMI-1-1 --off --output DP-1-2 --off
+# nvidia-settings --assign CurrentMetaMode=(nvidia-settings -q CurrentMetaMode -t|tr '\n' ' '|sed -e 's/.*:: \(.*\)/\1\n/g' -e 's/}/, ForceCompositionPipeline = On}/g')
