24114c2c9b00d397021158694fca321579f8455d
Xavi Campa <javi@campalus.com>
2022-08-12 23:53:40 +0200

    remove graphql lsp

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -53,10 +53,6 @@ local lsp_flags = {
     -- This is the default in Nvim 0.7+
     debounce_text_changes = 150,
 }
-require('lspconfig')['graphql'].setup {
-    on_attach = on_attach,
-    flags = lsp_flags,
-}
 require('lspconfig')['tsserver'].setup {
     on_attach = on_attach,
     flags = lsp_flags,
