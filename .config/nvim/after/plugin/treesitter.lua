7914c9eb614cebccd1e2aa994485a591e61674ca
Xavi Campa <xavi@campalus.com>
2023-01-23 10:21:22 +0100

    typescript tree-sitter

--- .config/nvim/after/plugin/treesitter.lua
+++ .config/nvim/after/plugin/treesitter.lua
@@ -1,6 +1,6 @@
 require 'nvim-treesitter.configs'.setup {
     -- A list of parser names, or "all"
-    ensure_installed = { "python", "lua", "yaml", "graphql", "javascript", "json", "go", "nix" },
+    ensure_installed = { "python", "lua", "yaml", "graphql", "javascript", "json", "go", "nix", "tsx", "typescript" },
 
     -- Install parsers synchronously (only applied to `ensure_installed`)
     sync_install = false,
