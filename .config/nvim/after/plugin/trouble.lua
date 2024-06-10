cacfe3786f4dbd406e852c3e90f855809261e420
Xavi Campa <xavi@campalus.com>
2024-06-10 10:13:16 +0200

    nvim trouble focus

--- .config/nvim/after/plugin/trouble.lua
+++ .config/nvim/after/plugin/trouble.lua
@@ -1,6 +1,6 @@
-vim.api.nvim_set_keymap("n", "<leader>xx", "<cmd>Trouble diagnostics toggle<cr>", {silent = true, noremap = true})
+vim.api.nvim_set_keymap("n", "<leader>xx", "<cmd>Trouble diagnostics toggle focus=true<cr>", {silent = true, noremap = true})
 vim.api.nvim_set_keymap("n", "<leader>xX", "<cmd>Trouble diagnostics toggle filter.buf=0<cr>", {silent = true, noremap = true})
-vim.api.nvim_set_keymap("n", "<leader>cs", "Trouble symbols toggle focus=false<cr>", {silent = true, noremap = true})
+vim.api.nvim_set_keymap("n", "<leader>cs", "<cmd>Trouble symbols toggle focus=false<cr>", {silent = true, noremap = true})
 vim.api.nvim_set_keymap("n", "<leader>cl", "<cmd>Trouble lsp toggle focus=false win.position=right<cr>", {silent = true, noremap = true})
 vim.api.nvim_set_keymap("n", "<leader>xL", "<cmd>Trouble loclist toggle<cr>", {silent = true, noremap = true})
 vim.api.nvim_set_keymap("n", "<leader>xQ", "<cmd>Trouble qflist toggle<cr>", {silent = true, noremap = true})
