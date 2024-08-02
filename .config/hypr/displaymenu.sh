bc6b7ddb5079a436aac06db3eec553045a341827
F. Javier L. Campa <javier.campa@webstep.no>
2024-08-02 12:18:38 +0200

    displays menu hyprland

---
+++ .config/hypr/displaymenu.sh
@@ -0,0 +1,13 @@
+op=$( echo -e "󰍹 DisplayPort\n󰍹 HDMI\n󰍺 Both" | rofi -i -dmenu --width 300 --height 200 | awk '{print tolower($2)}' )
+
+case $op in 
+  displayport)
+    hyprctl dispatch dpms off HDMI-A-1 && hyprctl dispatch dpms on DP-1
+    ;;
+  hdmi)
+    hyprctl dispatch dpms on HDMI-A-1 && hyprctl dispatch dpms off DP-1
+    ;;
+  both)
+    hyprctl dispatch dpms on HDMI-A-1 && hyprctl dispatch dpms on DP-1
+    ;;
+esac
