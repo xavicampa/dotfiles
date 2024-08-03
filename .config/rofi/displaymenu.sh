c53c80c7006bca5e30908169434b7e7c24d6ce3e
F. Javier L. Campa <javier.campa@webstep.no>
2024-08-03 09:33:45 +0200

    corrected displaymenu rofi

--- .config/rofi/displaymenu.sh
+++ .config/rofi/displaymenu.sh
@@ -1,13 +1,16 @@
-op=$( echo -e "󰶐 󰍹 DisplayPort\n󰍹 󰶐 HDMI\n󰍹 󰍹 Both" | rofi -i -dmenu | awk '{print tolower($2)}' )
+op=$( echo -e "󰶐 󰍹 DisplayPort\n󰍹 󰶐 HDMI\n󰍹 󰍹 Both" | rofi -i -dmenu | awk '{print tolower($3)}' )
 
 case $op in 
   displayport)
-    hyprctl dispatch dpms off HDMI-A-1 && hyprctl dispatch dpms on DP-1
+    hyprctl dispatch dpms off HDMI-A-1
+    hyprctl dispatch dpms on DP-1
     ;;
   hdmi)
-    hyprctl dispatch dpms on HDMI-A-1 && hyprctl dispatch dpms off DP-1
+    hyprctl dispatch dpms on HDMI-A-1
+    hyprctl dispatch dpms off DP-1
     ;;
   both)
-    hyprctl dispatch dpms on HDMI-A-1 && hyprctl dispatch dpms on DP-1
+    hyprctl dispatch dpms on HDMI-A-1
+    hyprctl dispatch dpms on DP-1
     ;;
 esac
