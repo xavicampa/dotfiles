2366aac1b017b3bc179b2c85cf59ddc3f674425b
Xavi Campa <xavi@campalus.com>
2024-09-11 16:09:42 +0200

    markdown plugin nvim

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -157,5 +157,12 @@ require("lazy").setup({
                 extract = "```$filetype\n(.-)```"
             }
         end,
+    },
+    {
+        'MeanderingProgrammer/render-markdown.nvim',
+        opts = {},
+        -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
+        -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
+        dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
     }
 })
