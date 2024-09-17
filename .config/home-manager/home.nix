45a9a0150232d5553095e52991ff55b9f47e2ac2
Xavi Campa <xavi@campalus.com>
2024-09-17 11:10:08 +0200

    kitty rounded corners

--- .config/home-manager/home.nix
+++ .config/home-manager/home.nix
@@ -159,7 +159,7 @@ in
         background_blur = 24;
         tab_bar_style = "powerline";
         tab_powerline_style = "slanted";
-        hide_window_decorations = "yes";
+        hide_window_decorations = "titlebar-only";
       };
       /* theme = "Gruvbox Dark"; */
       # theme = "Catppuccin-Macchiato";
