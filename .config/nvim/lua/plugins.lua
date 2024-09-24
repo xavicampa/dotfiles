f459a845f2da3b1c01445d91d5bd317ae7b8f425
Xavi Campa <xavi@campalus.com>
2024-09-24 09:33:22 +0200

    nvim avante (commented)

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -158,9 +158,50 @@ require("lazy").setup({
             }
         end,
     },
+    -- {
+    --     "yetone/avante.nvim",
+    --     dependencies = {
+    --         "stevearc/dressing.nvim",
+    --         "nvim-lua/plenary.nvim",
+    --         "MunifTanjim/nui.nvim",
+    --         "nvim-tree/nvim-web-devicons",
+    --     },
+    --     opts = {
+    --         provider = "ollama",
+    --         vendors = {
+    --             ---@type AvanteProvider
+    --             ollama = {
+    --                 ["local"] = true,
+    --                 endpoint = "127.0.0.1:11434/v1",
+    --                 model = "codegemma:2b-v1.1",
+    --                 parse_curl_args = function(opts, code_opts)
+    --                     return {
+    --                         url = opts.endpoint .. "/chat/completions",
+    --                         headers = {
+    --                             ["Accept"] = "application/json",
+    --                             ["Content-Type"] = "application/json",
+    --                         },
+    --                         body = {
+    --                             model = opts.model,
+    --                             messages = require("avante.providers").copilot.parse_message(code_opts), -- you can make your own message, but this is very advanced
+    --                             max_tokens = 2048,
+    --                             stream = true,
+    --                         },
+    --                     }
+    --                 end,
+    --                 parse_response_data = function(data_stream, event_state, opts)
+    --                     require("avante.providers").openai.parse_response(data_stream, event_state, opts)
+    --                 end,
+    --             },
+    --         },
+    --     }
+    -- },
     {
         'MeanderingProgrammer/render-markdown.nvim',
-        opts = {},
+        opts = {
+            file_types = { "markdown", "Avante" },
+        },
+        ft = { "markdown", "Avante" },
         -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
         -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
         dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
