074be82b7970839db9bd8270055a7bf1c71f7681
F. Javier L. Campa <javier.campa@webstep.no>
2025-01-13 16:12:30 +0100

    hyprland/nixos new display

--- .config/nixos/homepc/hardware-configuration.nix
+++ .config/nixos/homepc/hardware-configuration.nix
@@ -119,55 +119,55 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    extraConfig.pipewire."91-combined-sinks" = {
-      "context.modules" = [
-        {
-          name = "libpipewire-module-combine-stream";
-          args = {
-            combine.mode = "sink";
-            node.name = "combined_sink";
-            node.description = "My Combined Sink";
-            combine.latency-compensate = false;
-            combine.props = {
-              audio.position = [ "FL" "FR" ];
-            };
-            stream.props = {
-              stream.dont-remix = true;
-            };
-            stream.rules = [
-              {
-                matches = [
-                  {
-                    media.class = "Audio/Sink";
-                    node.name = "alsa_output.pci-0000_01_00.1.pro-output-3";
-                  }
-                ];
-                actions = {
-                  create-stream = {
-                    audio.position = [ "FL" "FR" ];
-                    combine.audio.position = [ "FL" "FR" ];
-                  };
-                };
-              }
-              {
-                matches = [
-                  {
-                    media.class = "Audio/Sink";
-                    node.name = "alsa_output.pci-0000_01_00.1.pro-output-7";
-                  }
-                ];
-                actions = {
-                  create-stream = {
-                    audio.position = [ "FL" "FR" ];
-                    combine.audio.position = [ "FL" "FR" ];
-                  };
-                };
-              }
-            ];
-          };
-        }
-      ];
-    };
+    # extraConfig.pipewire."91-combined-sinks" = {
+    #   "context.modules" = [
+    #     {
+    #       name = "libpipewire-module-combine-stream";
+    #       args = {
+    #         combine.mode = "sink";
+    #         node.name = "combined_sink";
+    #         node.description = "My Combined Sink";
+    #         combine.latency-compensate = false;
+    #         combine.props = {
+    #           audio.position = [ "FL" "FR" ];
+    #         };
+    #         stream.props = {
+    #           stream.dont-remix = true;
+    #         };
+    #         stream.rules = [
+    #           {
+    #             matches = [
+    #               {
+    #                 media.class = "Audio/Sink";
+    #                 node.name = "alsa_output.pci-0000_01_00.1.pro-output-3";
+    #               }
+    #             ];
+    #             actions = {
+    #               create-stream = {
+    #                 audio.position = [ "FL" "FR" ];
+    #                 combine.audio.position = [ "FL" "FR" ];
+    #               };
+    #             };
+    #           }
+    #           {
+    #             matches = [
+    #               {
+    #                 media.class = "Audio/Sink";
+    #                 node.name = "alsa_output.pci-0000_01_00.1.pro-output-7";
+    #               }
+    #             ];
+    #             actions = {
+    #               create-stream = {
+    #                 audio.position = [ "FL" "FR" ];
+    #                 combine.audio.position = [ "FL" "FR" ];
+    #               };
+    #             };
+    #           }
+    #         ];
+    #       };
+    #     }
+    #   ];
+    # };
   };
 
   systemd.sleep.extraConfig = ''
