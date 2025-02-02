805bf565894c4c868eb7c6e3d1b571c2fc0e7e23
F. Javier L. Campa <javier.campa@webstep.no>
2025-02-02 19:36:09 +0100

    remove gaps and waybar nixos, adjust displays rr

--- .config/rofi/displaymenu.sh
+++ .config/rofi/displaymenu.sh
@@ -3,7 +3,7 @@ op=$( echo -e "󰶐 󰍹 Only32\n󰍹 󰶐 Only27\n󰍹 󰍹 Both" | rofi -i -dm
 case $op in 
   only32)
     hyprctl keyword monitor "HDMI-A-1,disable"
-    hyprctl keyword monitor "DP-2,highrr,2560x0,1.2,vrr,1"
+    hyprctl keyword monitor "DP-2,highrr,2560x0,1.2,vrr,0"
     ;;
   only27)
     hyprctl keyword monitor "HDMI-A-1,preferred,0x0,1"
@@ -11,6 +11,6 @@ case $op in
     ;;
   both)
     hyprctl keyword monitor "HDMI-A-1,preferred,0x0,1"
-    hyprctl keyword monitor "DP-2,highrr,2560x0,1.2,vrr,1"
+    hyprctl keyword monitor "DP-2,3840x2160@120,2560x0,1.2,vrr,0"
     ;;
 esac
