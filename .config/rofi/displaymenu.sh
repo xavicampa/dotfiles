1d1884029ad67583100274e47a2b8c967aafd989
F. Javier L. Campa <javier.campa@webstep.no>
2025-02-02 13:26:06 +0100

    display config hyprland

--- .config/rofi/displaymenu.sh
+++ .config/rofi/displaymenu.sh
@@ -2,15 +2,15 @@ op=$( echo -e "󰶐 󰍹 Only32\n󰍹 󰶐 Only27\n󰍹 󰍹 Both" | rofi -i -dm
 
 case $op in 
   only32)
-    hyprctl keyword monitor "DP-3,disable"
-    hyprctl keyword monitor "HDMI-A-1,3840x2160@120,2560x0,1.2,vrr,1"
+    hyprctl keyword monitor "HDMI-A-1,disable"
+    hyprctl keyword monitor "DP-2,highrr,2560x0,1.2,vrr,1"
     ;;
   only27)
-    hyprctl keyword monitor "DP-3,preferred,0x0,1"
-    hyprctl keyword monitor "HDMI-A-1,disable"
+    hyprctl keyword monitor "HDMI-A-1,preferred,0x0,1"
+    hyprctl keyword monitor "DP-2,disable"
     ;;
   both)
-    hyprctl keyword monitor "DP-3,preferred,0x0,1"
-    hyprctl keyword monitor "HDMI-A-1,3840x2160@120,2560x0,1.2,vrr,1"
+    hyprctl keyword monitor "HDMI-A-1,preferred,0x0,1"
+    hyprctl keyword monitor "DP-2,highrr,2560x0,1.2,vrr,1"
     ;;
 esac
