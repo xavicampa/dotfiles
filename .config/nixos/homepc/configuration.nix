043481268b004a380b087f08ea0f7fa8af9e90b2
F. Javier L. Campa <javier.campa@webstep.no>
2025-03-08 17:33:05 +0100

    added swap for homepc

--- .config/nixos/homepc/configuration.nix
+++ .config/nixos/homepc/configuration.nix
@@ -57,7 +57,12 @@
       fsType = "vfat";
     };
 
-  swapDevices = [ ];
+  swapDevices = [
+    {
+      device = "/swapfile";
+      size = 16 * 1024; # 32GB
+    }
+  ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
