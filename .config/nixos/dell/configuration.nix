dfdc1cb0031a1cd4df202fe44b73328160d66098
F. Javier Campa L. <javi@campalus.com>
2025-03-10 21:53:32 +0100

    dell scroll and swap

--- .config/nixos/dell/configuration.nix
+++ .config/nixos/dell/configuration.nix
@@ -9,7 +9,7 @@
 }:
 
 # let
-#   stable = import <nixpkgs-stable> { config.allowUnfree = true; };
+#   unstable = import <nixpkgs-unstable> { config.allowUnfree = true; };
 # in
 
 {
@@ -52,7 +52,13 @@
       fsType = "vfat";
     };
 
-  swapDevices = [ ];
+  swapDevices = [
+    {
+      device = "/swapfile";
+      size = 16 * 1024; # 32GB
+    }
+  ];
+  boot.kernel.sysctl."vm.swappiness" = 0;
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
