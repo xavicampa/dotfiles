8183c66ebecd52301272d8dbcf55a15c3704f97d
Xavi Campa <xavi@campalus.com>
2025-05-25 21:57:50 +0200

    nvim format map

--- .config/nvim/lua/remap.lua
+++ .config/nvim/lua/remap.lua
@@ -6,3 +6,6 @@ vim.api.nvim_set_keymap("i", "jk", "<ESC>", opts)
 -- Navigate diagnostics
 vim.keymap.set('n', '<C-p>', function() vim.diagnostic.jump({ count = -1 }) end, opts)
 vim.keymap.set('n', '<C-n>', function() vim.diagnostic.jump({ count = 1 }) end, opts)
+
+-- Formatting
+vim.keymap.set('n', '<Space>f', function() vim.lsp.buf.format() end, opts)
