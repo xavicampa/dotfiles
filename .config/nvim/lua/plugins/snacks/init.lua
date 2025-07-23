f5968a2758d6705f8b55ef7f7f91a0a4dbfaf69c
Xavi Campa <xavi@campalus.com>
2025-07-23 12:57:51 +0200

    nvim snacks

--- .config/nvim/lua/plugins/snacks/init.lua
+++ .config/nvim/lua/plugins/snacks/init.lua
@@ -1,11 +1,17 @@
 return {
     "folke/snacks.nvim",
     keys = {
-        {"<leader>gg", "<cmd>lua Snacks.lazygit.open()<CR>"},
+        { "<leader>gg", function () Snacks.lazygit.open() end, desc = "Open lazygit" },
     },
+    lazy = false,
     opts = {
+        animation = {},
+        dashboard = {},
+        dim = {},
+        indent = {},
         lazygit = {},
         scroll = {},
+        terminal = {},
     },
     priority = 1000,
 }
