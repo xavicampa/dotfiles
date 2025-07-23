772a628176714422d30dc113589eb5096501dccb
F. Javier L. Campa <javier.campa@webstep.no>
2025-07-23 14:14:33 +0200

    qwen2.5 for nvim

--- .config/nvim/lua/plugins/llm/init.lua
+++ .config/nvim/lua/plugins/llm/init.lua
@@ -1,26 +1,39 @@
 return {
-        'huggingface/llm.nvim',
-        opts = {
-            backend = "ollama",
-            accept_keymap = "<S-Tab>",
-            model = "codellama:7b-code",
-            fim = {
-                enabled = true,
-                prefix = "<PRE> ",
-                middle = " <MID>",
-                suffix = " <SUF>",
-            },
-            context_window = 4096,
-            url = "http://localhost:11434",
-            tokens_to_clear = { "<EOT>" },
-            -- model = "deepseek-coder-v2:16b-lite-base-q4_0",
-            -- fim = {
-            --     enabled = true,
-            --     prefix = "<｜fim▁begin｜>",
-            --     middle = "<｜fim▁end｜>",
-            --     suffix = "<｜fim▁hole｜>",
-            -- },
-            -- context_window = 4096,
-            -- url = "http://localhost:11434",
-        }
+    'huggingface/llm.nvim',
+    opts = {
+        backend = "ollama",
+        accept_keymap = "<S-Tab>",
+        url = "http://localhost:11434",
+
+        -- model = "codellama:7b-code",
+        -- fim = {
+        --     enabled = true,
+        --     prefix = "<PRE> ",
+        --     middle = " <MID>",
+        --     suffix = " <SUF>",
+        -- },
+        -- context_window = 4096,
+        -- tokens_to_clear = { "<EOT>" },
+
+        model = "qwen2.5-coder:1.5b-base",
+        fim = {
+            enabled = true,
+            prefix = "<|fim_prefix|>",
+            middle = "<|fim_middle|>",
+            suffix = "<|fim_suffix|>",
+        },
+        context_window = 32768,
+        -- tokenizer = {
+        --     repository = "Qwen/Qwen2.5-Coder-1.5B"
+        -- },
+
+        -- model = "deepseek-coder-v2:16b-lite-base-q4_0",
+        -- fim = {
+        --     enabled = true,
+        --     prefix = "<｜fim▁begin｜>",
+        --     middle = "<｜fim▁end｜>",
+        --     suffix = "<｜fim▁hole｜>",
+        -- },
+        -- context_window = 4096,
+    }
 }
