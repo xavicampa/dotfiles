24854d6097f5d9b93bd25e538dada284bea46e9b
Xavi Campa <xavi@campalus.com>
2025-08-05 19:31:28 +0200

    nvim lsp init

---
+++ .config/nvim/lua/plugins/lsp/init.lua
@@ -0,0 +1,45 @@
+return {
+    "neovim/nvim-lspconfig",
+    init = function()
+        -- Configure default global language servers
+        vim.lsp.config("lua_ls", {
+            settings = {
+                Lua = {
+                    runtime = { version = "LuaJIT" },
+                    diagnostics = { enable = true },
+                    telemetry = { enable = false },
+                    workspace = {
+                        library = { vim.env.VIMRUNTIME },
+                        checkThirdParty = false,
+                    },
+                },
+            }
+        })
+        vim.lsp.config("nixd", {
+            settings = {
+                nixd = {
+                    nixpkgs = {
+                        expr = "import <nixpkgs> { }",
+                    },
+                    formatting = {
+                        command = { "nixfmt" },
+                    },
+                    options = {
+                        home_manager = {
+                            expr =
+                            "(import <home-manager/modules> { configuration = ~/.config/home-manager/home.nix; pkgs = import <nixpkgs> {}; }).options"
+                        }
+                    }
+                }
+            }
+        })
+
+        -- Enable LSPs
+        vim.lsp.enable(
+            {
+                "nixd",
+                "lua_ls",
+            }
+        )
+    end,
+}
