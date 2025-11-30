9bf24800beed13450ab2e567cca308835a32c1a6
Javier Campa <javier.campa@webstep.no>
2025-11-30 19:02:23 +0100

    opencode and llama.vim settings for llama.cpp

--- .config/nvim/lua/plugins/llm/init.lua
+++ .config/nvim/lua/plugins/llm/init.lua
@@ -2,7 +2,15 @@ return {
   "ggml-org/llama.vim",
   init = function()
     vim.g.llama_config = {
-      show_info = 0
+      show_info = 0,
+
+      -- endpoint = "https://router.huggingface.co/v1"
+      endpoint = "http://localhost:8082/infill",
+
+      -- replace the default keymaps with custom ones
+      keymap_accept_full = "<C-j>",
+      keymap_accept_line = "<C-l>",
+      keymap_accept_word = "<C-w>",
     }
   end
 }
