6718c8604c266a6b1bfae1fc00641b39ef989679
Javier Campa <javier.campa@webstep.no>
2026-01-28 23:37:12 +0100

    kitty font size 12pt homepc

--- .config/home-manager/home.nix
+++ .config/home-manager/home.nix
@@ -129,7 +129,7 @@ in {
           "JetBrainsMono Nerd Font Mono"
         else
           "JetBrainsMono Nerd Font";
-        size = if macos then 16 else 11;
+        size = if macos then 16 else 12;
       };
       keybindings = { "ctrl+shift+h" = "previous_window"; };
       settings = {
