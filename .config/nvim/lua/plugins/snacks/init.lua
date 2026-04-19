d230e3ea7b1fd5bcfef6661f6f64f4e1944d6bc2
Javier Campa <javier.campa@webstep.no>
2026-04-19 19:09:27 +0200

    snacks explorer behavior

--- .config/nvim/lua/plugins/snacks/init.lua
+++ .config/nvim/lua/plugins/snacks/init.lua
@@ -1,36 +1,52 @@
 return {
-    "folke/snacks.nvim",
-    keys = {
-        { "<leader><space>", function() Snacks.picker.smart() end,           desc = "Smart Find Files" },
-        { "<leader>,",       function() Snacks.picker.buffers() end,         desc = "Buffers" },
-        { "<leader>/",       function() Snacks.picker.grep() end,            desc = "Grep" },
-        { "<leader>:",       function() Snacks.picker.command_history() end, desc = "Command History" },
-        { "<leader>n",       function() Snacks.picker.notifications() end,   desc = "Notification History" },
-        { "<leader>e",       function() Snacks.explorer() end,               desc = "File Explorer" },
-        { "<leader>g",       function() Snacks.lazygit.open() end,           desc = "Open lazygit" },
+  "folke/snacks.nvim",
+  keys = {
+    { "<leader><space>", function() Snacks.picker.smart() end,           desc = "Smart Find Files" },
+    { "<leader>,",       function() Snacks.picker.buffers() end,         desc = "Buffers" },
+    { "<leader>/",       function() Snacks.picker.grep() end,            desc = "Grep" },
+    { "<leader>:",       function() Snacks.picker.command_history() end, desc = "Command History" },
+    { "<leader>n",       function() Snacks.picker.notifications() end,   desc = "Notification History" },
+    {
+      "<leader>e",
+      function()
+        local explorer_pickers = Snacks.picker.get({ source = "explorer" })
+        for _, v in pairs(explorer_pickers) do
+          if v:is_focused() then
+            v:close()
+          else
+            v:focus()
+          end
+        end
+        if #explorer_pickers == 0 then
+          Snacks.picker.explorer()
+        end
+      end,
+      desc = "File Explorer"
     },
-    lazy = false,
-    opts = {
-        animation = {},
-        explorer = {},
-        image = {},
-        indent = {},
-        input = {},
-        lazygit = {},
-        notifier = {},
-        picker = {
-            sources = {
-                explorer = {
-                    auto_close = true,
-                    win = {
-                        input = { keys = { ["<C-t>"] = { "tab", mode = { "n", "i" } }, }, },
-                        list = { keys = { ["<C-t>"] = { "tab", mode = { "n", "i" } }, }, },
-                    },
-                }
-            }
-        },
-        scroll = {},
-        terminal = {},
+    { "<leader>g", function() Snacks.lazygit.open() end, desc = "Open lazygit" },
+  },
+  lazy = false,
+  opts = {
+    animation = {},
+    explorer = {
     },
-    priority = 1000,
+    image = {},
+    indent = {},
+    input = {},
+    lazygit = {},
+    notifier = {},
+    picker = {
+      sources = {
+        explorer = {
+          win = {
+            input = { keys = { ["<C-t>"] = { "tab", mode = { "n", "i" } }, }, },
+            list = { keys = { ["<C-t>"] = { "tab", mode = { "n", "i" } }, }, },
+          },
+        }
+      }
+    },
+    scroll = {},
+    terminal = {},
+  },
+  priority = 1000,
 }
