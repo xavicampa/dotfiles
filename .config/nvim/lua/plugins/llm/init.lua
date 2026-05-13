9097efd5c10352b6c663592269f723108f68740d
Javier Campa <javier.campa@webstep.no>
2026-05-13 10:45:08 +0200

    homepc llm agent updates

--- .config/nvim/lua/plugins/llm/init.lua
+++ .config/nvim/lua/plugins/llm/init.lua
@@ -5,9 +5,12 @@ return {
     vim.g.llama_config = {
       show_info = 0,
 
-      endpoint_fim = "http://localhost:8080/upstream/Qwen3.6-35B-A3B-GGUF/infill",
-      -- endpoint_fim = "http://localhost:8080/upstream/Qwen2.5-Coder-14B-GGUF/infill",
-      endpoint_inst = "http://localhost:8080/upstream/Qwen3.6-35B-A3B-GGUF/v1/chat/completions",
+      -- endpoint_fim = "http://localhost:8080/upstream/Qwen3.6-35B-A3B-GGUF/infill",
+      endpoint_fim = "http://localhost:8012/infill",
+
+      endpoint_inst = "http://localhost:8080/v1/chat/completions",
+      -- model_inst = "Qwen3.6-35B-A3B-GGUF"
+      model_inst = "Qwen3.6-27B-GGUF"
     }
   end
 }
