3b7a28cc8635bb2b328b21485bc71a9d8b461f16
Javier Campa <javier.campa@webstep.no>
2026-05-25 18:46:17 +0200

    update nvim llm plugin to use qwen36 27B model and local infill endpoint

--- .config/nvim/lua/plugins/llm/init.lua
+++ .config/nvim/lua/plugins/llm/init.lua
@@ -3,13 +3,13 @@ return {
   init = function()
 
     vim.g.llama_config = {
+
       show_info = 0,
 
-      -- endpoint_fim = "http://localhost:8080/upstream/Qwen3.6-35B-A3B-GGUF/infill",
-      endpoint_fim = "http://localhost:8012/infill",
+      endpoint_fim = "http://localhost:8080/infill",
+      model_fim = "Qwen3.6-27B-GGUF",
 
       endpoint_inst = "http://localhost:8080/v1/chat/completions",
-      -- model_inst = "Qwen3.6-35B-A3B-GGUF"
       model_inst = "Qwen3.6-27B-GGUF"
     }
   end
