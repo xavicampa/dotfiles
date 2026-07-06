7a3839eccb81b851995020d3a56325a421828d86
Javier Campa <javi@campalus.com>
2026-07-06 22:13:11 +0200

    llm: switch from Qwopus3.6-27B-v2 to Qwen3.6-27B-MTP

--- .config/nvim/lua/plugins/llm/init.lua
+++ .config/nvim/lua/plugins/llm/init.lua
@@ -7,10 +7,10 @@ return {
       show_info = 0,
 
       endpoint_fim = "http://localhost:8080/infill",
-      model_fim = "Jackrong/Qwopus3.6-27B-v2-MTP-GGUF:Q8_0",
+      model_fim = "unsloth/Qwen3.6-27B-MTP-GGUF:Q6_K_XL",
 
       endpoint_inst = "http://localhost:8080/v1/chat/completions",
-      model_inst = "Jackrong/Qwopus3.6-27B-v2-MTP-GGUF:Q8_0"
+      model_inst = "unsloth/Qwen3.6-27B-MTP-GGUF:Q6_K_XL"
     }
   end
 }
