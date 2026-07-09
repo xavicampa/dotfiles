5082f44175e84b6815bded4aeefd4c94804d84aa
Javier Campa <javi@campalus.com>
2026-07-09 09:14:58 +0200

    nvim: update none-ls config

--- .config/nvim/lua/plugins/none-ls/init.lua
+++ .config/nvim/lua/plugins/none-ls/init.lua
@@ -8,11 +8,11 @@ return {
         require("null-ls").builtins.formatting.black,
         require("null-ls").builtins.formatting.prettier,
         require("null-ls").builtins.diagnostics.cfn_lint.with({
-          --     extra_args = {
-          -- '-i', 'W3002', -- Do not try to parse nested stack's TemplateURL
-          -- '-i', 'E3043', -- Do not try to parse nested stack's TemplateURL
-          -- '-c', 'I', --  Include suggestions and best practices
-          --     }
+              extra_args = {
+                '-i', 'W3002', -- Do not try to parse nested stack's TemplateURL
+                -- '-i', 'E3043', -- Do not try to parse nested stack's TemplateURL
+                -- '-c', 'I', --  Include suggestions and best practices
+              }
         }),
         require("null-ls").builtins.diagnostics.checkstyle.with({
           args = function(params)
