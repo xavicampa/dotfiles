71e16d9eb555e897511ab7e5576cde8129401b12
Javier Campa <javi@campalus.com>
2026-07-13 10:18:16 +0200

    nixos: disable USB autosuspend to prevent context state warnings

--- .config/nixos/homepc/configuration.nix
+++ .config/nixos/homepc/configuration.nix
@@ -96,6 +96,7 @@
       # "nvidia-drm.fbdev=1"
       # "mem_sleep_default=deep"
       "xhci_hcd.quirks=0x80"  # RESET_ON_RESUME: fix xHC error in resume (USBSTS 0x401/0x411)
+      "usbcore.autosuspend=-1" # disable USB autosuspend to prevent context state warnings
     ];
     kernelPackages = pkgs.linuxPackages_latest;
     blacklistedKernelModules = [ "spd5118" ];
