3d78e9753b3219ac47ab1bfd3ad7af5e5a9f56dc
Javier Campa <javi@campalus.com>
2026-07-17 12:45:49 +0200

    Switch to bottlecapai/ThinkingCap-Qwen3.6-27B-GGUF:Q8_0 model

--- .config/nvim/lua/plugins/llm/init.lua
+++ .config/nvim/lua/plugins/llm/init.lua
@@ -7,10 +7,10 @@ return {
       show_info = 0,
 
       endpoint_fim = "http://localhost:8080/infill",
-      model_fim = "unsloth/Qwen3.6-27B-MTP-GGUF:Q8_0",
+      model_fim = "bottlecapai/ThinkingCap-Qwen3.6-27B-GGUF:Q8_0",
 
       endpoint_inst = "http://localhost:8080/v1/chat/completions",
-      model_inst = "unsloth/Qwen3.6-27B-MTP-GGUF:Q8_0"
+      model_inst = "bottlecapai/ThinkingCap-Qwen3.6-27B-GGUF:Q8_0"
     }
   end
 }
