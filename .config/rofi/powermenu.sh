10faa8e99a8492b1c867d25df69cf5104ca74c43
Javier Campa <javi@campalus.com>
2026-09-17 12:35:26 +0200

    powermenu: add reboot-to-linux option (efibootmgr 0000)

--- .config/rofi/powermenu.sh
+++ .config/rofi/powermenu.sh
@@ -1,4 +1,4 @@
-op=$( echo -e " Suspend\n Poweroff\n󰜉 Reboot\n Logout\n Reboot-to-Windows\n Reboot-to-UEFI" | rofi -i -dmenu | awk '{print tolower($2)}' )
+op=$( echo -e " Suspend\n Poweroff\n󰜉 Reboot\n Logout\n󱄅 Reboot-to-Linux\n Reboot-to-Windows\n Reboot-to-UEFI" | rofi -i -dmenu | awk '{print tolower($2)}' )
 
 case $op in 
   poweroff)
@@ -10,6 +10,9 @@ case $op in
     ;;
   logout)
     hyprctl dispatch exit 1
+    ;;
+  reboot-to-linux)
+    pkexec efibootmgr -n 0000 && reboot
     ;;
   reboot-to-windows)
     pkexec efibootmgr -n 0002 && reboot
