5257dd5d5288cb55397a0552aea4e36a58cc8f9c
Javier Campa <javi@campalus.com>
2026-09-25 21:10:02 +0200

    llamacpp+nixos(homepc): add GSQ-RCO IQ3_S preset; expose GPU 1 first (CUDA_VISIBLE_DEVICES=1,0) and tensor-split 14,24

--- .config/nixos/homepc/configuration.nix
+++ .config/nixos/homepc/configuration.nix
@@ -304,6 +304,7 @@ in
             --network host \
             --device nvidia.com/gpu=all \
             -e GGML_CUDA_ALLREDUCE=internal \
+            -e CUDA_VISIBLE_DEVICES=1,0 \
             -v /home/javi/.cache/huggingface:/root/.cache/huggingface \
             -v /home/javi/.config/llamacpp/llama-preset.ini:/app/llama-preset.ini:ro \
             localhost/llama-cuda:mtmd-fix \
